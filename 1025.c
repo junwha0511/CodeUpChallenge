c444f7708cf85916430a49944e5fdce2633d75b8
준화 <junwha0511@naver.com>
2018-03-10 14:51:43 +0900

    .

--- 1025.c
+++ 1025.c
@@ -4,9 +4,9 @@
 int main(){
 	int num[5],i;
 	scanf("%1d%1d%1d%1d%1d",&num[0],&num[1],&num[2],&num[3],&num[4]);
-	printf("[%d]\n",10000*num[4]);
-	printf("[%d]\n",1000*num[3]);
+	printf("[%d]\n",10000*num[0]);
+	printf("[%d]\n",1000*num[1]);
 	printf("[%d]\n",100*num[2]);
-	printf("[%d]\n",10*num[1]);
-	printf("[%d]\n",1*num[0]);
+	printf("[%d]\n",10*num[3]);
+	printf("[%d]\n",1*num[4]);
 }
